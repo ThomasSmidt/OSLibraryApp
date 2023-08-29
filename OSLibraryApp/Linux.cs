e91ee62ceb816452df1af51c247dc7424565e9ae
ThomasSmidt <smidt592@gmail.com>
2023-08-29 08:39:10 +0200

    Add project files.

---
+++ OSLibraryApp/Linux.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibraryApp
+{
+    internal class Linux : OS, IOS
+    {
+        public Linux(string userFullName) : base(userFullName)
+        {
+        }
+        public override string ShowOS()
+        {
+            return "Welcome to Linux!";
+        }
+    }
+}
---
+++ OSLibraryApp/OS.cs
@@ -0,0 +1,15 @@
+﻿namespace OSLibraryApp
+{
+    public class OS : IOS
+    {
+        public string UserFullName { get; set; }
+        public OS(string userFullName)
+        {
+            UserFullName = userFullName;
+        }
+        public virtual string ShowOS()
+        {
+            return "No valid operating system detected";
+        }
+    }
+}
